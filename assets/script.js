0eee864a8f1edb7c751186aec91128c80c554744
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-03 17:09:36 +0100

    fix: aggiustato stampaggio in pagina

--- assets/script.js
+++ assets/script.js
@@ -6,21 +6,31 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const emailrandom = [];
 
 for(let i = 0; i < 10; i++){
-  fetch(endpoint)
-  .then(response => response.json())
-  .then(data => {          
-       emailrandom.push(data.response);      
-  })
+  //  fetch(endpoint)
+  // .then(response => response.json())
+  // .then(data => {          
+  //      emailrandom.push(data.response);
 
-  if(emailrandom[i] === 10){
-    document.getElementById('emailgen').innerHTML = emailrandom;
-  }else{
-    document.getElementById('errormsg').className ='';
-  }
+  //      if(emailrandom.length >= 10){
+  //       document.getElementById('emailgen').innerHTML = emailrandom;      
+        
+  //     }
+             
+  // }) 
+
+  axios.get(endpoint)
+  
+  .then(response => {          
+       emailrandom.push(response.data.response);
+
+       if(emailrandom.length >= 10){
+        document.getElementById('emailgen').innerHTML = emailrandom;      
+        
+      }
+             
+  }) 
       
 }
-
-
 
 console.log(emailrandom);
 

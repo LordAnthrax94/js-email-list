41f5b26acb7a23c9ec5891d322e8952e3ebcc2f9
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-03 16:51:06 +0100

    fix: impostato ciclo for e condizione per stampa in pagina

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,27 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 const emailrandom = [];
 
-fetch(endpoint)
+for(let i = 0; i < 10; i++){
+  fetch(endpoint)
   .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    
+  .then(data => {          
+       emailrandom.push(data.response);      
   })
+
+  if(emailrandom[i] === 10){
+    document.getElementById('emailgen').innerHTML = emailrandom;
+  }else{
+    document.getElementById('errormsg').className ='';
+  }
+      
+}
+
+
+
+console.log(emailrandom);
+
+
+  
+
+
 
